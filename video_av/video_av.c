4ce52f259fec3a831f0e8db8adbc092098b017fc
Pierre Parent <pierre.parent@pparent.fr>
2017-11-30 12:15:39 +0100

    New way to do things, more reliable smoothing

--- video_av/video_av.c
+++ video_av/video_av.c
@@ -37,7 +37,7 @@ extern int prop_set_rawiso_approx(unsigned iso);
 
 void FAST digital_gain_simulate_virtual_iso (int current_virtual_iso,int current_frame_iso)
 {
-            if (smooth_changes)
+            if (smooth_changes && current_virtual_iso!=current_frame_iso)
             {
             set_movie_digital_iso_gain_for_gradual_expo(1024.0f * powf(2, -1.0f*(current_frame_iso-current_virtual_iso)/8.0f));
             }
@@ -45,13 +45,12 @@ void FAST digital_gain_simulate_virtual_iso (int current_virtual_iso,int current
               set_movie_digital_iso_gain_for_gradual_expo(1024.0f);  
 }
 
-int tv_step(int *current_tv,int desired_tv,int *shutter_iso_compensation, int * current_virtual_iso)
+int tv_step(int *current_tv,int desired_tv)
 {
          
         if ((*current_tv)<0x60 || (*current_tv)>0x98)
              return 0;
              
-        int previous_tv=(*current_tv);
          int diff=(*current_tv)-(int)COERCE(desired_tv,0x60, 0x98);
          
          //we don't need to change tv
@@ -60,117 +59,79 @@ int tv_step(int *current_tv,int desired_tv,int *shutter_iso_compensation, int * 
                 return 0;
          }
          
-         int change_made=0;
-         int h;
-         //If possible jump of 8: it is faster and smoother because real ISO compensation cannot
-         //take every value but can take every value % 4;
-
-         int dontcompensate=0;
-                
-         //If possible jump of as many 4: it is faster and smoother because real ISO compensation cannot
-         //take every value but can take every value % 4;
-         for (h=32;h>=4;h-=4)
-         {
-            if (!change_made)
-                {
-                if(diff<-(h-1) && MAX_ISO-(*current_virtual_iso)>h)
-                        {(*current_tv)=(*current_tv)+h;change_made=1;}
-                if(diff>(h-1)  &&  (*current_virtual_iso)-MIN_ISO>h )
-                        {(*current_tv)=(*current_tv)-h;change_made=1;}
-                }
-         }
-         
          //If we could not achieve a 4 step then do a 1 step
-//          if (!change_made)
-//          {
-//          if(diff<0 && ABS((*shutter_iso_compensation))<4 && MAX_ISO-(*current_virtual_iso)>0 )
-//             {(*current_tv)=(*current_tv)+1;dontcompensate=1;}
-//          if(diff>0  && ABS((*shutter_iso_compensation))<4 && (*current_virtual_iso)-MIN_ISO>0 )
-//             {(*current_tv)=(*current_tv)-1;;dontcompensate=1;}
-//          }
-//        
-         //Ensure that we have an acceptable value
-         (*current_tv)=COERCE((*current_tv), 0x60, 0x98); 
+
+         if(diff<0)
+            (*current_tv)=(*current_tv)+1;
+         if(diff>0   )
+            (*current_tv)=(*current_tv)-1;
          
-         if ((*current_virtual_iso)+(*current_tv)-previous_tv>=MIN_ISO &&
-             (*current_virtual_iso)+(*current_tv)-previous_tv<=MAX_ISO)
-         {
-        //set rawshutter and shutter iso compensation
-        lens_info.raw_shutter=(*current_tv);
-         (*current_virtual_iso)+=(*current_tv)-previous_tv;
-            
-         set_movie_digital_iso_gain_for_gradual_expo(1024.0f);
-            bv_set_rawiso(*current_virtual_iso);
-            bv_set_rawshutter((*current_tv)); 
-            
-            if ((*current_tv)-previous_tv!=0)
-                while(read_frame_iso()!=(*current_virtual_iso));
-                  msleep(10);
-           msleep(50);
-                shutter_iso_compensation=0;
-//               bmp_printf(FONT_MED,100,150,"test1  %d %d",(*current_tv)-previous_tv,(*shutter_iso_compensation));
-            return 1;
-         }
+         bv_set_rawshutter((*current_tv));
          
-         return 0;
-        
-         
+         return 1;
+                 
 }
 
-void iso_step(int *current_virtual_iso,int current_frame_iso,int *last_requested_iso,int desired_iso,int * shutter_iso_compensation, int *virtual_iso_at_last_request)
+void iso_step(int *current_iso,int *last_requested_iso,int desired_iso)
 {
         unsigned analog_iso,digital_gain,value_to_set;
-        static int wait_for_set=0;
-        
-        if ((*last_requested_iso)==current_frame_iso)
-            wait_for_set=0;
-        
-        int previous_virtual_iso=(*current_virtual_iso);
+
         
         if (desired_iso !=0)
         {
             //Increase or decrease virtual iso in the desired direction
-            if((*current_virtual_iso)-desired_iso <0 )
-                        (*current_virtual_iso)++;
+            if((*current_iso)-desired_iso <0 )
+                        (*current_iso)++;
 
-            if((*current_virtual_iso)-desired_iso >0 )
-                        (*current_virtual_iso)--;
+            if((*current_iso)-desired_iso >0 )
+                        (*current_iso)--;
         }
-        
-         
-        //Ensure that frame iso is not at more than 8 distance of virtual iso
-          while ( (*current_virtual_iso)-current_frame_iso > 8)
-              (*current_virtual_iso)--;
-          while ( (*current_virtual_iso)-current_frame_iso <-8)
-              (*current_virtual_iso)++;      
-          
+                 
           //The value we should try to set physically to the sensor
           //Is our virtual iso, plus compensation for shutter.
-            value_to_set=(*current_virtual_iso);
+            value_to_set=(*current_iso);
           
           value_to_set=COERCE(value_to_set,MIN_ISO,MAX_ISO);
           
           //If the value to set is a full analog ISO, then request to set it
           split_iso(value_to_set, &analog_iso, &digital_gain);
-          if (digital_gain==0 || smooth_changes==0 ||  (*shutter_iso_compensation)!=0  )
-          {
-              if (!wait_for_set||!smooth_changes)
-              {
+           if (digital_gain==0 || smooth_changes==0  )
+           {
                 bv_set_rawiso(value_to_set);
-//                 bmp_printf(FONT_MED,200,350,"test2  %d %d %d",(*current_virtual_iso),*shutter_iso_compensation,value_to_set);
                 (*last_requested_iso)=value_to_set;
-                (*virtual_iso_at_last_request)=(*current_virtual_iso);
-                wait_for_set=1;
-              }
-          }
+           }
 }
 
+int  virtual_expo_step(int *current_virtual_expo,int desired_expo,int current_hard_expo)
+{
+    int diff1=(*current_virtual_expo)-desired_expo;
+    int diff2=(*current_virtual_expo)-current_hard_expo;
+    
+    if (diff1<0 && diff2<8)
+        {(*current_virtual_expo)++; return 0;}
+    if (diff1>0 && diff2>-8)
+        {(*current_virtual_expo)--; return 0;}
+        
+//     if (diff1<0 && diff2<8  && rand()%4==0)
+//         {(*current_virtual_expo)++; return 0;}
+//     if (diff1>0 && diff2>-8  && rand()%4==0 )
+//         {(*current_virtual_expo)--; return 0;}   
+        
+    if ( diff2>10)
+         {(*current_virtual_expo)--; return 0;}
+         
+    if ( diff2<-10)
+         {(*current_virtual_expo)++; return 0;}       
+         
+        return 1;
+}
 
-#define AE_SPEED 60
+#define AE_SPEED 32
+#define JUMP_EXPO 16
 
 static void FAST video_av_task()
 {
-    int current_virtual_iso,current_frame_iso,last_requested_iso,desired_iso,virtual_iso_at_last_request;
+    int current_iso,current_frame_iso,last_requested_iso,desired_iso,virtual_iso_at_last_request;
     int current_tv, desired_tv, shutter_iso_compensation=0;
     video_av_running = 1;
     
@@ -179,67 +140,39 @@ static void FAST video_av_task()
     int t2=get_ms_clock_value(),t3=t2;
     //Init values       
     desired_iso=lens_info.raw_iso_ae; 
-    current_frame_iso=read_frame_iso();last_requested_iso=0;current_virtual_iso=lens_info.raw_iso;
+    current_frame_iso=read_frame_iso();last_requested_iso=0;current_iso=lens_info.raw_iso;
     desired_tv=lens_info.raw_shutter_ae;current_tv=desired_tv;
     virtual_iso_at_last_request=lens_info.raw_iso;
     unsigned int kk=0;
     int last_frame_iso;
+    int frame_tv,current_hard_tv;
+    int current_hard_expo,current_virtual_expo,desired_expo;
+    
+    current_hard_expo=lens_info.raw_iso-lens_info.raw_shutter;
+    current_virtual_expo=lens_info.raw_iso-lens_info.raw_shutter;
     
     TASK_LOOP
     {
         if (  !gui_menu_shown() && av_expo_enabled && expo_override_active() &&  shooting_mode == SHOOTMODE_MOVIE )
         {
+        //Start of main loop in activated mode    
             
-        t0 = get_ms_clock_value();
-        
-        current_frame_iso=read_frame_iso();
-        if (t0-t1>=AE_SPEED)
-        {
-      
-            //Update desired values given by canon AE
-            if (desired_iso==0)
-            {
-            desired_iso=(int)(lens_info.raw_iso_ae);
-            desired_tv=lens_info.raw_shutter_ae;
-            }
-            kk++;
-            bmp_printf(FONT_MED,300,300,"TV XXXXXXXXXXXXX");
-             bmp_printf(FONT_MED,300,300,"TV %d %d %d",current_tv,desired_tv,shutter_iso_compensation);
-             
-            bmp_printf(FONT_MED,50,50,"ISO XXXXXXXXXXXXX");
-             bmp_printf(FONT_MED,50,50,"ISO %d %d %d %d",current_frame_iso,current_virtual_iso,last_requested_iso,desired_iso);
-             
-            if( current_frame_iso == last_requested_iso && shutter_iso_compensation==0 && current_virtual_iso==current_frame_iso && current_virtual_iso==desired_iso)
-            {
-                tv_step(&current_tv,desired_tv,&shutter_iso_compensation,&current_virtual_iso);
-                current_frame_iso=read_frame_iso();
-                last_requested_iso=current_frame_iso;
-            }
-            else
-            iso_step(&current_virtual_iso,current_frame_iso,&last_requested_iso,desired_iso,
-                     &shutter_iso_compensation,&virtual_iso_at_last_request);
- 
-            t1=t0;
-            
-        }
-                
-        
+        current_frame_iso=read_frame_iso();                
+        if( current_frame_iso == last_requested_iso) digital_gain_simulate_virtual_iso(current_virtual_expo,last_requested_iso-current_tv);
         
         //If last requested values have been applied to sensor
         if( current_frame_iso == last_requested_iso || current_frame_iso == 0 || smooth_changes==0 )
             {
-            //Reset shutter compensation when values where applied;
-            current_virtual_iso+=shutter_iso_compensation;
-//             if(shutter_iso_compensation!=0)
-//                 bmp_printf(FONT_MED,0,0,"test3  %d %d %d",current_frame_iso,last_requested_iso,current_virtual_iso );
-            digital_gain_simulate_virtual_iso(current_virtual_iso,current_frame_iso);
-            shutter_iso_compensation=0;
+            digital_gain_simulate_virtual_iso(current_virtual_expo,last_requested_iso-current_tv);    
+            current_hard_expo=last_requested_iso-current_tv;
+            current_hard_tv=current_tv;
 
             last_frame_iso=current_frame_iso;
       
             //Update desired values given by canon AE
             desired_iso=(int)(lens_info.raw_iso_ae);
-            desired_tv=lens_info.raw_shutter_ae;
+            desired_tv=(int)COERCE(lens_info.raw_shutter_ae,0x60, 0x98);
+            desired_expo=desired_iso-desired_tv;
             
             //Update Av value if needed
             av_value=COERCE(av_value,lens_info.raw_aperture_min,lens_info.raw_aperture_max);
@@ -247,29 +180,75 @@ static void FAST video_av_task()
                 lens_set_rawaperture(av_value);
             
             
-            if (current_virtual_iso==0)
-                current_virtual_iso=(int)(lens_info.raw_iso);
+            if (current_iso==0)
+                current_iso=(int)(lens_info.raw_iso);
             
             //We can rest and wait because there won't be any value change
             //In FRAME_ISO which could provoke glitch.
             msleep(AE_SPEED);
             }
-            else 
-            { 
-                digital_gain_simulate_virtual_iso(current_virtual_iso,current_frame_iso); 
+
+                
+            
+            
+           t0 = get_ms_clock_value();  
+        if (t0-t1>=AE_SPEED)
+        {
+      
+            //Update desired values given by canon AE
+            if (desired_iso==0)
+            {
+            desired_iso=(int)(lens_info.raw_iso_ae);
+            desired_tv=(int)COERCE(lens_info.raw_shutter_ae,0x60, 0x98);
+            desired_expo=desired_iso-desired_tv;
             }
+            kk++;
+            bmp_printf(FONT_MED,300,300,"TV XXXXXXXXXXXXX");
+             bmp_printf(FONT_MED,300,300,"TV %d %d",current_tv,desired_tv);
+             
+            bmp_printf(FONT_MED,50,50,"ISO XXXXXXXXXXXXX");
+             bmp_printf(FONT_MED,50,50,"ISO %d %d %d %d",current_frame_iso,current_iso,last_requested_iso,desired_iso);
+             
+            bmp_printf(FONT_MED,150,150,"EXPO XXXXXXXXXXXXX");
+             bmp_printf(FONT_MED,150,150,"EXPO %d %d %d",current_virtual_expo,current_hard_expo,desired_expo);
+             kk++;
+            
+             if (kk%2==0  || !smooth_changes)
+             {
+                if(last_requested_iso==last_frame_iso|| ABS(last_requested_iso-current_iso)<7)
+                    iso_step(&current_iso,&last_requested_iso,desired_iso);
+                if ( last_requested_iso!=last_frame_iso || kk%8==0 || !smooth_changes )
+                {
+                    int oldtv=current_tv;
+                    tv_step(&current_tv,desired_tv);
+                    if(last_requested_iso==last_frame_iso)
+                        current_virtual_expo-=(current_tv-oldtv);
+
+                }
+                
+                if (last_requested_iso!=last_frame_iso || current_tv==desired_tv)
+                virtual_expo_step(&current_virtual_expo,desired_expo,current_hard_expo);
+             }
+ 
+            t1=t0;
+            
+        }   
+            digital_gain_simulate_virtual_iso(current_virtual_expo,current_hard_expo); 
         
+        //End of main loop in activated mode    
         }
         else
         {
             msleep(5000);
-            current_virtual_iso=lens_info.raw_iso;
+            current_iso=lens_info.raw_iso;
             last_requested_iso=lens_info.raw_iso;
             current_frame_iso=lens_info.raw_iso;
             current_tv=lens_info.raw_shutter;
             shutter_iso_compensation=0;
             virtual_iso_at_last_request=lens_info.raw_iso;
-            digital_gain_simulate_virtual_iso(current_virtual_iso,current_frame_iso);
+            current_hard_expo=lens_info.raw_iso-lens_info.raw_shutter;
+            current_virtual_expo=lens_info.raw_iso-lens_info.raw_shutter;
+            digital_gain_simulate_virtual_iso(current_virtual_expo,current_hard_expo);
         }
     }
     
